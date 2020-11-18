ac0747c477e63595dba649b37890dda5bdb17b42
Yasuo78 <45259890+Yasuo78@users.noreply.github.com>
2020-11-18 17:08:41 +0300

    Task_02

---
+++ RAP_Task_02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RAP_Task_02
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string exit_Word;//переменная для чтения текста
+
+            Console.WriteLine("Данная программа выполняется пока вы не введёте слово 'exit'");
+
+            do//цикл
+
+            {
+
+                Console.WriteLine(">");
+
+                exit_Word = Convert.ToString(Console.ReadLine());
+
+            } while (exit_Word != "exit");//условие завершения - пользователь ввёл exit
+
+            Console.WriteLine('\n');
+
+            Console.WriteLine("Завершение программы...");
+
+        }
+    }
+}
